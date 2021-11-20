5f0483f465bba1fc61e99c9f7abe8c5e8ff73c16
yeonha99 <rhdqn1207@naver.com>
2021-11-20 19:35:36 +0900

    fix : jwt 커스텀 필터 기능 구현

---
+++ src/main/java/com/example/Gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,68 @@
+package com.example.Gateway.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    Environment env;
+
+    public AuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    } //생성자 주이비
+
+    public static class Config {}
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {//로그인 안한 사람이 접근 시
+                return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);//토큰 없다고 401 보냄
+            }
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", ""); //Bearer toekn으로 담아져 와서 변환하는 과정
+            if (!isJwtValid(jwt)) return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+
+            return chain.filter(exchange);
+        };
+
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = true;
+        String subject = null;
+        try {
+            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception ex) {
+            log.info("jwt 변환 중 예외 발생");
+            returnValue = false;
+        }
+        if (subject == null || subject.isEmpty()) returnValue = false;
+
+        return returnValue;
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return response.setComplete();
+    }
+}
