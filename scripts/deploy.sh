08fcc72e6c687313854dde841eedc72d761af994
EC2 Default User <ec2-user@ip-172-16-2-117.ap-northeast-2.compute.internal>
2021-12-03 09:43:04 +0900

    fix: 포트번호 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 sudo docker ps -a -q --filter "name=gateway" | grep -q . && docker stop gateway && docker rm gateway | true
 sudo docker rmi 0826hihi/up3-gateway-test:latest
 sudo docker pull 0826hihi/up3-gateway-test:latest
-docker run -d -p 8000:8000 --name gateway 0826hihi/up3-gateway-test:latest
+docker run -d -p 9000:9000 --name gateway 0826hihi/up3-gateway-test:latest
 docker rmi -f $(docker images -f "dangling=true" -q) || true
