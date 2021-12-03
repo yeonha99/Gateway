5f3fbb1a1fa25672f586e204bf7299b6ed087ed0
yeonha99 <rhdqn1207@naver.com>
2021-12-03 21:45:51 +0900

    fix: restTemplate

---
+++ src/main/java/com/example/gateway/Config.java
@@ -0,0 +1,19 @@
+package com.example.gateway;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config
+{
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+}
