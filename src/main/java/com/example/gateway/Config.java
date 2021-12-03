3bede26252bb4f7835748be5d2ac357bec9e1fa2
yeonha99 <rhdqn1207@naver.com>
2021-12-04 00:34:31 +0900

    feat: 웹 클라이언트 어찌고 저찌고 추가

--- src/main/java/com/example/gateway/Config.java
+++ src/main/java/com/example/gateway/Config.java
@@ -1,19 +1,26 @@
 package com.example.gateway;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
 
 @Configuration
 public class Config
 {
 
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @Primary
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
-
+// 비동기어찌고 논블럭이찌고 저찌고
 
 }
