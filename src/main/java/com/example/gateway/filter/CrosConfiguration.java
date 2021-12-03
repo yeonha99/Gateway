fc767a217d31e070c3cfcc7860eb58628387edaf
yeonha99 <rhdqn1207@naver.com>
2021-12-03 21:01:52 +0900

    fix: 설정

--- src/main/java/com/example/gateway/filter/CrosConfiguration.java
+++ src/main/java/com/example/gateway/filter/CrosConfiguration.java
@@ -15,8 +15,8 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class CrosConfiguration {
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
+    private static final String ALLOWED_HEADERS = "*";
+    private static final String ALLOWED_METHODS = "*";
     private static final String ALLOWED_ORIGIN = "*";
     private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
 
