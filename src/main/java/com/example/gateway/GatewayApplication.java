14b692e31f84da0d92c28cc79b3bf235b57e1d74
yeonha99 <rhdqn1207@naver.com>
2021-12-05 02:17:02 +0900

    fix: 어노테이션 추가

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
